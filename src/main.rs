5adda7f9a1821cccc7c5c488999c436b964d10cd
Alex-Bolduc <avec2342@live.ca>
2025-04-26 19:07:19 -0400

    turso db setup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use libsql::Builder;
+
 fn main() {
     println!("Hello, world!");
 
-    println!("test")
+    let url = std::env::var("TURSO_DATABASE_URL").expect("TURSO_DATABASE_URL must be set");
+    let token = std::env::var("TURSO_AUTH_TOKEN").expect("TURSO_AUTH_TOKEN must be set");
 }
