87c3bab19115584d8b4e8f7e3a78f20f4bb5ee07
Alex-Bolduc <avec2342@live.ca>
2025-06-11 00:28:40 -0400

    Implemented GET /characters request

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,68 @@
+use axum::extract::State;
 use futures::StreamExt;
+use handlers::characters;
 use libsql::de::from_row;
 use libsql::{Builder, Error};
 use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+mod handlers;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    // Setting up DB
     dotenv::dotenv().ok();
-
-    let router = axum::Router::new().route("/", axum::routing::get(get_handler));
-
-    let address: &'static str = "0.0.0.0:3000";
-    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
-
     let db_url = std::env::var("TURSO_DATABASE_URL").expect("TURSO DATABASE URL not set");
     let db_token = std::env::var("TURSO_AUTH_TOKEN").expect("TURSO DATABASE TOKEN not set");
 
-    let mut db = Builder::new_remote_replica("local.db", db_url, db_token)
+    let db = Builder::new_remote_replica("local.db", db_url, db_token)
         .build()
         .await
         .unwrap();
 
-    let conn = db.connect()?;
+    let connection = db.connect()?;
 
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS characters (
+    // Creating DB if it doesn't already exist
+    connection
+        .execute(
+            "CREATE TABLE IF NOT EXISTS characters (
         name TEXT PRIMARY KEY,
         class TEXT NOT NULL CHECK (class IN ('warrior', 'mage', 'ranger')),
         gold INTEGER NOT NULL CHECK (gold >= 0)
         )",
-        (),
-    )
-    .await?;
+            (),
+        )
+        .await?;
 
-    let mut test = conn.query("SELECT * FROM characters", ()).await.unwrap();
-    let characters: Vec<Character> = into_rows(test).await;
-    println!("{:?}", characters);
+    // Server
+
+    let state = AppState { conn: connection };
+
+    let router = axum::Router::new()
+        .route("/characters", axum::routing::get(get_characters))
+        .with_state(state);
+
+    let address: &'static str = "0.0.0.0:3000";
+    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
 
     axum::serve(listener, router).await.unwrap();
 
     Ok(())
 }
 
-async fn get_handler() {
-    println!("Hello World!")
+#[axum::debug_handler]
+async fn get_characters(State(state): State<AppState>) {
+    let query = state
+        .conn
+        .query("SELECT * FROM characters", ())
+        .await
+        .unwrap();
+    let characters: Vec<Character> = into_rows(query).await;
+    println!("{:?}", characters);
+}
+
+#[derive(Clone)]
+struct AppState {
+    conn: libsql::Connection,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
